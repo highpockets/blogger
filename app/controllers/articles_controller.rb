562352ef1f1fdd6f95a7814beebca35270322760
Kris Tobiasson <kris.tobiasson@gmail.com>
2019-10-18 22:51:51 +0200

    Reset to comments and cleaned project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,9 +20,6 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
-
-        @comment = Comment.new
-        @comment.article_id = @article.id
     end
 
     def edit
